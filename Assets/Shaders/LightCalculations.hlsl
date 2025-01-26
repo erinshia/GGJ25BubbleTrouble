397da1bb584ebcbfac3944e91c659c9597da227d
s.perezalonso <s.perezalonso@k5factory.com>
2025-01-26 10:10:35 +0100

    Add a separate HLSL file for light calculations

---
+++ Assets/Shaders/LightCalculations.hlsl
@@ -0,0 +1,24 @@
+﻿#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/RealtimeLights.hlsl"
+
+inline half3 CalculateLightAmount(half4 positionOS, half3 normalWS)
+{
+    // Get the VertexNormalInputs of the vertex, which contains the normal in world space
+    VertexNormalInputs positions = GetVertexNormalInputs(positionOS);
+
+    // Main light
+    Light light = GetMainLight();
+    half3 lightAmount = LightingLambert(light.color, light.direction, positions.normalWS.xyz);
+
+    // Add additional lights
+    uint additionalLightCount = GetAdditionalLightsCount();
+    for (uint i = 0; i < additionalLightCount; i++)
+    {
+        Light additionalLight = GetAdditionalLight(i, TransformObjectToWorld(positionOS));
+        lightAmount += LightingLambert(additionalLight.color, additionalLight.direction, normalWS) *
+            additionalLight.distanceAttenuation * additionalLight.shadowAttenuation;
+    }
+
+    return saturate(lightAmount);
+}
