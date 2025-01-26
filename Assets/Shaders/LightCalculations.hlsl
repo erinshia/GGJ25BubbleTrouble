53ef82df345a8b2a59e05e5b6f3ca7735392c88d
s.perezalonso <s.perezalonso@k5factory.com>
2025-01-26 11:08:44 +0100

    Clean up lighting files

--- Assets/Shaders/LightCalculations.hlsl
+++ Assets/Shaders/LightCalculations.hlsl
@@ -2,6 +2,11 @@
 #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
 #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/RealtimeLights.hlsl"
 
+inline half3 CalculateAmbientLight(half3 normalWS)
+{
+    return SampleSH(normalWS);
+}
+
 inline half3 CalculateLightAmount(half4 positionOS, half3 normalWS)
 {
     // Get the VertexNormalInputs of the vertex, which contains the normal in world space
@@ -10,6 +15,8 @@ inline half3 CalculateLightAmount(half4 positionOS, half3 normalWS)
     // Main light
     Light light = GetMainLight();
     half3 lightAmount = LightingLambert(light.color, light.direction, positions.normalWS.xyz);
+    half3 ambient = CalculateAmbientLight(normalWS);
+    lightAmount += ambient;
 
     // Add additional lights
     uint additionalLightCount = GetAdditionalLightsCount();
@@ -23,14 +30,21 @@ inline half3 CalculateLightAmount(half4 positionOS, half3 normalWS)
     return saturate(lightAmount);
 }
 
-half3 SampleReflection(half4 positionCS, half3 positionWS, half3 worldNormal, half3 viewDir, half smoothness = 1,
+// inline half3 SampleSpecular(half3 lightDirection, half3 viewDirWS)
+// {
+//     float3 halfwayDir = normalize(lightDirection + viewDirWS);
+//     float spec = pow(saturate(dot(surfaceNormal, halfwayDir)), smoothness * 128.0);
+//     return spec * lightColor;
+// }
+
+half3 SampleReflection(half4 positionCS, half3 positionWS, half3 worldNormal, half3 viewDir, half smoothness,
                        half occlusion = 1)
 {
     // Compute the reflection direction
     half3 reflectionDir = reflect(-viewDir, worldNormal);
 
     // half perceptualRoughness = RoughnessToPerceptualRoughness(1.0 - smoothness);
-    half perceptualRoughness = RoughnessToPerceptualRoughness(0.5); // Replace 0.5 with smoothness property
+    half perceptualRoughness = RoughnessToPerceptualRoughness(1.0 - smoothness);
     half2 screenUV = GetNormalizedScreenSpaceUV(positionCS);
 
 
